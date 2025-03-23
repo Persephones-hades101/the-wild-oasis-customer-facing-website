06dcec78c0e072928fd9632e0a82a3bb8972a07e
sudh4800@gmail.com <sudh4800@gmail.com>
2025-03-23 22:03:10 +0530

    creating a new user on first signin

--- app/account/page.js
+++ app/account/page.js
@@ -1,15 +1,18 @@
 import Link from 'next/link';
+import { auth } from '../_lib/auth';
 
 export const metadata = {
   title: 'Account',
   description: 'View your account information here.',
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(' ')[0];
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome , User
+        Welcome, {firstName}
       </h2>
       <p>Welcome back! You can view your account information here.</p>
       <Link href="/account/profile">View your profile</Link>
